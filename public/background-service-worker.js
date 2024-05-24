43754747e53ac5dd2b89af7c2cdfd4e657bbd526
Gellé Matthieu <matthieu.gelle@agence-web-france.com>
2024-05-24 14:02:33 +0200

    first commit

---
+++ public/background-service-worker.js
@@ -0,0 +1,35 @@
+let uniq = (a) => [...new Set(a)];
+
+const addVideo = (url) => {
+  chrome.storage.local.get("videos", function (data) {
+    const currentVideos = data.videos || [];
+    chrome.storage.local.set({ videos: uniq([...currentVideos, url]) });
+  });
+};
+
+const handleVideoRequest = (details) => {
+  addVideo(details.url);
+};
+
+chrome.webRequest.onCompleted.addListener(
+  function (details) {
+    if (details.url.includes("m3u8")) {
+      handleVideoRequest(details);
+    }
+  },
+  { urls: ["<all_urls>"] }
+);
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "getVideos") {
+    chrome.storage.local.get("videos", function (data) {
+      sendResponse({ videos: data.videos || [] });
+    });
+    return true;
+  }
+  if (message.action === "clearVideos") {
+    chrome.storage.local.set({ videos: [] });
+    sendResponse({ videos: [] });
+    return true;
+  }
+});
---
+++ src/components/App/App.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [videos, setVideos] = useState<string[]>([]);
+
+  const getVideos = () => {
+    chrome.runtime.sendMessage({ action: "getVideos" }, function (response) {
+      setVideos(response.videos);
+    });
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const clearVideos = () => {
+    chrome.runtime.sendMessage({ action: "clearVideos" }, function (response) {
+      setVideos(response.videos);
+    });
+  };
+
+  const copyToClipboard = (url: string) => {
+    navigator.clipboard.writeText(url).then(
+      () => {
+        alert("Lien copié dans le presse-papiers");
+      },
+      (err) => {
+        console.error("Erreur lors de la copie du lien: ", err);
+      }
+    );
+  };
+
+  return (
+    <main className="p-4 w-[500px] h-[500px] relative overflow-y-auto bg-neutral-950 text-neutral-50">
+      <div className="flex items-center justify-between">
+        <span className="inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs text-white ring-1 ring-inset ring-gray-800 text-opacity-50">
+          <svg
+            className="h-1.5 w-1.5 fill-green-400 animate-pulse"
+            viewBox="0 0 6 6"
+            aria-hidden="true"
+          >
+            <circle cx={3} cy={3} r={3} />
+          </svg>
+          Analyse du réseau (en arrière plan)
+        </span>
+      </div>
+      <ul className="mt-6 space-y-2 mb-12">
+        {videos.length > 0 && (
+          <li className="fixed right-0 bottom-0 px-4 py-2 flex gap-2 items-center justify-end bg-neutral-900 border-t border-indigo-500 w-full">
+            <p>{videos.length} résultat(s)</p>
+            <button
+              className="rounded bg-white/10 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-white/20"
+              onClick={clearVideos}
+            >
+              Vider la liste
+            </button>
+          </li>
+        )}
+        {videos.map((url) => (
+          <li
+            className="max-w-full block break-all cursor-pointer hover:blur-sm"
+            key={url}
+            onClick={() =>
+              copyToClipboard(
+                `ffmpeg -i "${url}" -bsf:a aac_adtstoasc -c copy output.mp4`
+              )
+            }
+          >
+            {url}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default App;
